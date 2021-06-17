87758ddcb661a11cca286d527e4510d43e1a9670
Barbara Pereira <barbara.pereira1990@gmail.com>
2021-06-16 21:17:01 -0300

    criacao de variaveis e tipo de operacoes

---
+++ primeiros-passos-com-javascript/primeiros-passos/variaveisEOperacoes.js
@@ -0,0 +1,23 @@
+//Declaração de variáveis
+
+var nome = 'Bárbara';
+const idade = '31';
+
+//Operações Aritiméticas
+
+console.log(2 + 2);
+console.log(2 + 3 * 8);
+console.log((2 + 3) * 8);
+
+//concatenação de texto com valor
+console.log(`ano ${2021}`);
+
+//conversão de texto para número
+console.log('2' + '2');
+console.log(parseInt('2') + parseInt('2')); //conversao explicita
+console.log('2' - '2');//conversão implícita
+console.log('2' * '2');
+console.log('2' / '2');
+console.log('Bárbara' / '2');
+console.log(2.5);
+console.log(2,5);
