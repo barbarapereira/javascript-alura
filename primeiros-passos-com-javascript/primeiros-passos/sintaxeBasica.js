ae1b40d8762d9d900688c30ca6a80cf555e9c5dc
Barbara Pereira <barbara.pereira1990@gmail.com>
2021-06-16 21:16:31 -0300

    primeiros passos com js

---
+++ primeiros-passos-com-javascript/primeiros-passos/sintaxeBasica.js
@@ -0,0 +1,11 @@
+console.log('Meu Primeiro Programa');
+
+const idade = 30;
+const somaValor = 3;
+const idadeSomada = idade + somaValor;
+console.log(`minha idade: ${idade} anos.`);
+console.log(`idade somada: ${idadeSomada}`);
+
+//operações básicas com as variáveis
+console.log(`somando + 2: ${idade + 2} anos.`);
+
