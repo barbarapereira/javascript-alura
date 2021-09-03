e2b6781e28722acda8a718e28639a0ced22e4aec
Barbara Pereira <barbara.pereira1990@gmail.com>
2021-09-03 18:34:47 -0300

    trabalhando com atrubuições de variaveis

--- primeiros-passos-com-javascript/primeiros-passos/atribuicaoDeVariaveis.js
+++ primeiros-passos-com-javascript/primeiros-passos/atribuicaoDeVariaveis.js
@@ -4,3 +4,10 @@ console.log(nome + ' ' + sobrenome);
 console.log(`${nome} ${sobrenome}`);
 const nomeCompleto = `${nome} ${sobrenome}`;
 console.log(nomeCompleto);
+
+let contador = 0;
+contador - contador + 1;
+
+let idade;//declarando variável
+idade = 31; // atribuindo variavel
+console.log('idade', idade = idade + 1);
