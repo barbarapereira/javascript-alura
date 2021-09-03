33fe5ec67a92c793345caee858017193cb19bbc4
Barbara Pereira <barbara.pereira1990@gmail.com>
2021-09-03 19:29:38 -0300

    criando validações  iterativas

--- primeiros-passos-com-javascript/primeiros-passos/7-condicionais.js
+++ primeiros-passos-com-javascript/primeiros-passos/7-condicionais.js
@@ -10,17 +10,16 @@ console.log(`Trabalhando com Condicionais`)
 const idadeComprador = 14;
 const estaAcompanhada = true;
 const acompanhante = 25;
-if (idadeComprador < 18) {
-  if (estaAcompanhada && acompanhante >= 18) {
-    console.log(`Pode viajar, mas com o acompanhante. Passagem comprada para ${listaDeDestinos[1]}`);
-    listaDeDestinos.splice(1, 1);
-  } else {
-    console.log('Menor de idade, não posso vender passagem');
-  }
-}
-else {
-  console.log(`Comprador maior de idade, comprou passagem para ${listaDeDestinos[1]}`);
-  listaDeDestinos.splice(1, 1);
-}
+const destino = listaDeDestinos[3]
+const removeDestino = listaDeDestinos.splice(3, 1);
 
+if (idadeComprador > 18) {
+  console.log(`Comprador maior de idade, comprou passagem para ${destino}`);
+  removeDestino;
+} else if (estaAcompanhada && acompanhante >= 18) {
+  console.log(`Pode viajar, mas com o acompanhante. Passagem comprada para ${destino}`);
+  removeDestino;
+} else {
+  console.log('Menor de idade, não posso vender passagem');
+}
 console.log(listaDeDestinos);
